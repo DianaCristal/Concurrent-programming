16ffc0c4a4de7fa110b74767e1fe8ef358224b5e
Mateusz Syska <251639@edu.p.lodz.pl>
2025-04-27 11:34:18 +0200

    Testy do warstwy Data

--- DataTest/BallUnitTest.cs
+++ DataTest/BallUnitTest.cs
@@ -23,14 +23,37 @@ namespace TP.ConcurrentProgramming.Data.Test
     [TestMethod]
     public void MoveTestMethod()
     {
-      Vector initialPosition = new(10.0, 10.0);
-      Ball newInstance = new(initialPosition, new Vector(0.0, 0.0));
-      IVector curentPosition = new Vector(0.0, 0.0);
-      int numberOfCallBackCalled = 0;
-      newInstance.NewPositionNotification += (sender, position) => { Assert.IsNotNull(sender); curentPosition = position; numberOfCallBackCalled++; };
-      newInstance.Move();
-      Assert.AreEqual<int>(1, numberOfCallBackCalled);
-      Assert.AreEqual<IVector>(initialPosition, curentPosition);
+        Vector initialPosition = new(10.0, 10.0);
+        Ball newInstance = new(initialPosition, new Vector(0.0, 0.0));
+        IVector curentPosition = new Vector(0.0, 0.0);
+        int numberOfCallBackCalled = 0;
+        newInstance.NewPositionNotification += (sender, position) => { Assert.IsNotNull(sender); curentPosition = position; numberOfCallBackCalled++; };
+        newInstance.Move();
+        Assert.AreEqual<int>(1, numberOfCallBackCalled);
+        Assert.AreEqual<IVector>(initialPosition, curentPosition);
+
+        Ball newerInstance = new(initialPosition, new Vector(10.0, 10.0));
+        IVector currenterPosition = new Vector(0.0, 0.0);
+        newerInstance.NewPositionNotification += (sender, position) => { Assert.IsNotNull(sender); currenterPosition = position; numberOfCallBackCalled++; };
+        newerInstance.Move();
+        Vector newPosition = new(20.0, 20.0);
+        Assert.AreEqual<IVector>(currenterPosition, newPosition);
+        Assert.AreEqual<int>(2, numberOfCallBackCalled);
+        }
+
+        [TestMethod]
+        public void SetPositionTestMethod()
+        {
+            Vector initialPosition = new Vector(10.0, 10.0);
+            Ball newInstance = new(initialPosition, new Vector(0.0, 0.0));
+            IVector curentPosition = new Vector(0.0, 0.0);
+            int numberOfCallBackCalled = 0;
+            newInstance.NewPositionNotification += (sender, position) => { Assert.IsNotNull(sender); curentPosition = position; numberOfCallBackCalled++; };
+
+            newInstance.SetPosition(new Vector(20.0, 20.0));
+
+            Assert.AreEqual<int>(1, numberOfCallBackCalled);
+            Assert.AreEqual<IVector>(new Vector(20.0, 20.0), curentPosition);
+        }
     }
-  }
 }
